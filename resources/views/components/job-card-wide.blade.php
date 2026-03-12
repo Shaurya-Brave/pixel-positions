b3c1cd55ee176e0b8be450100e929cdb5f7e5d20
Shaurya-Brave <braveop27@gmail.com>
2026-03-12 19:14:37 +0530

    Complete pixel positions project

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,26 @@
+@props(['job'])
+
+<x-panel class="flex flex-row gap-x-6">
+    <div>
+        <x-employer-logo/>
+    </div>
+
+    <div class="flex-1 felx flex-col">
+        <a href="" class="self-start text-sm text-gray-400">{{$job->employer->name }} </a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">
+            <a href="{{$job->url }}" target="_blank">{{$job->title }}</a>
+        </h3>
+
+        <p class="text-sm text-gray-400 mt-5">{{$job->salary }} </p>
+
+    </div>
+
+    <div>
+        @foreach ($job->tags as $tag)
+            <x-tag :tag=$tag />
+        @endforeach
+
+    </div>
+
+</x-panel>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-gray-900 text-white pb-10">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/40">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+
+            <div class="space-x-6 font-bold">
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+
+            @auth
+                <div>
+                    <a href="/jobs/create">Post a Job</a>
+                </div>
+            @endauth
+
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Login</a>
+                </div>
+            @endguest
+        </nav>
+        <main class="mt-10 max-w-[986px] mx-auto">
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+</html>
